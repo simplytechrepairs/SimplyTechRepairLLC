7117212065f3fc1aa5b79241e20fad1d07f5ed1f
Simply Tech Repairs LLC <owners@simplytechrepairs.com>
2024-11-20 22:16:43 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -57,10 +57,8 @@ window.mobileCheck = function () {
     $(".carousal-container").hide();
     // Change to mobile background wallpaper
     $("body").css({ "background-image": "url(wallpaper//mobile-background.svg)" });
-    $("body").css({ "background-position": "-225px" });
+    //$("body").css({ "background-position": "-225px" });
     $("body").css({ "background-size": "cover" });
-    // document.body.style.backgroundImage = url("mobile-background.svg");
-    //document.body.style.backgroundImage.css("background-repeat", "no-repeat");
 
     // Keep pricing table containers at 100% for mobile
     $(".pricing-tables-container .individual-service-pricing").css("width", "100%");
